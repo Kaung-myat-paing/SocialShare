77d774ca476106ae13be90f415f58d2469e31121
Kaung Myat Paing <software.frontdev2.cyberking@gmail.com>
2020-11-24 11:48:21 +0630

    "First Commit"

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Share Via:</h1>
+    <facebook :url="url" scale="3"></facebook>
+    <viber :url="url" scale="3"></viber>
+  </div>
+</template>
+
+<script>
+import Facebook from "./components/Facebook.vue";
+import Viber from "./components/Viber.vue";
+
+export default {
+  name: "app",
+  components: {
+    Facebook,
+    Viber
+  },
+  data() {
+    return {
+      url: "https://github.com/Kaung-myat-paing",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+  #app > span {
+    margin: 1em;
+  }
+</style>
---
+++ src/components/Viber.vue
@@ -0,0 +1,39 @@
+<template>
+  <span @click="viberLink(url)">
+    <icon name="brands/viber" color="#583eb5" :scale="scale"></icon>
+  </span>
+</template>
+
+<script>
+import objectToGetParams from "../utils/objectToGetParams";
+import "vue-awesome/icons/brands/viber";
+import Icon from "vue-awesome/components/Icon";
+export default {
+  name: "twitter",
+  methods: {
+    viberLink(url) {
+      window.open(
+        "viber://forward" +
+          objectToGetParams({
+            url: url,
+            text: url,
+          }),
+        "__blank"
+      );
+    },
+  },
+  components: {
+    Icon,
+  },
+  props: {
+    url: {
+      required: true,
+      type: String,
+    },
+    scale: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
